dd6842dd179309a2801cae8f58ed9001146ecbe1
Andrij.Lozunkyi <lozunskyi.andrij@gmail.com>
2017-08-20 15:10:40 +0300

    some changes

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+// set port
+var port = process.env.PORT || 5000;
+
+app.use( express.static( __dirname ) );
+
+app.get("/", function( req, res ) {
+	res.render("index");
+})
+
+app.listen( port, function () { 
+
+	console.log('Node app is running on port', port); 
+
+});
